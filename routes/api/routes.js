e2e4caf5a73781f7adbab04eabea35d62f74fb07
madyardwn <madyardwn@gmail.com>
2022-10-10 12:48:54 +0700

    arif

--- routes/api/routes.js
+++ routes/api/routes.js
@@ -1,13 +1,24 @@
 const { Router } = require("express");
 const controller = require("../../controller/controller");
+const multer = require("multer");
 
 const router = Router();
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + "-" + file.originalname);
+  },
+});
+
+const uploads = multer({ storage: fileStorage });
 
 // GET
 router.get("/", controller.getMitra);
 
 // POST
-router.post("/", controller.addMitra);
+router.post("/", uploads.single("logo_mitra"), controller.addMitra);
 
 // DELETE
 router.delete("/:id", controller.deleteMitra);
--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const multer = require("multer");
 
 const routes = require("./routes/api/routes");
 require("dotenv").config();
@@ -14,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(morgan("tiny"));
-app.use(bodyParser.json());
+app.use(express.json());
 
 mongoose
   .connect(process.env.DB_CONNECT, {
@@ -28,33 +26,10 @@ mongoose
     console.log("Koneksi ke database gagal", err);
   });
 
-const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, new Date().getTime() + "-" + file.originalname);
-  },
-});
-
-const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === "image/png" ||
-    file.mimetype === "image/jpg" ||
-    file.mimetype === "image/jpeg"
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
-
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
-);
+
 app.use("/uploads", express.static("uploads"));
 app.use("/", routes);
 
